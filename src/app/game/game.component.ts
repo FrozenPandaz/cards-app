def1b1616ce6f5d9779c1aa4fd236d85af67898f
Jason Jean <jjean@forbes.com>
2016-06-12 16:40:27 -0400

    Added list of users

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {RouteParams} from '@angular/router-deprecated';
-
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
 	moduleId: module.id,
@@ -9,10 +9,27 @@ import {RouteParams} from '@angular/router-deprecated';
 	styles: [require('./game.component.scss')]
 })
 export class GameComponent implements OnInit {
-	game_id: string;
+	game: any;
+	users: FirebaseListObservable<any[]>;
 
-	constructor(private routeParams: RouteParams) {
-		this.game_id = this.routeParams.get('gameId');
+	constructor(private routeParams: RouteParams, private af: AngularFire) {
+		let game_id = this.routeParams.get('gameId');
+		this.af.database.object('/games/' + game_id).map(game => {
+			game.name = game.name || game.$key;
+			return game;
+		}).subscribe(game => {
+			this.game = game;
+		});
+		this.users = <FirebaseListObservable<any[]>> this.af.database
+		.list('/games/' + game_id + '/users/', {
+			query: {
+				orderByChild: 'count'
+			}
+		}).map(users => {
+			return users.filter(user => {
+				return user.$key !== 'count';
+			});
+		});
 	}
 
 	ngOnInit() {
